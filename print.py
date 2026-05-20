a7094389dc3dc52d90a7be9660e97e7b8c78f7d6
isai <isai.massa@avahi.ai>
2026-05-20 15:38:41 -0600

    funcion al cuadrado

--- print.py
+++ print.py
@@ -1,1 +1,11 @@
 print("This is my first commit")
+
+print("Let's modify this file")
+
+numero = int(input("Qué numero quieres elevar al cuadradro? "))
+
+# Función que eleva un número al cuadrado
+def squared_n(n):
+    return n ** 2
+
+print(f"{numero} al cuadrado es {squared_n(numero)}")
